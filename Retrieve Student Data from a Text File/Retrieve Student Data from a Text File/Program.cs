8b5a964711975e867d2bb727fd577262a6e08519
deepakk2000 <deepakjc112000@gmal.com>
2023-11-21 18:11:13 +0530

    Retrieve data

---
+++ Retrieve Student Data from a Text File/Retrieve Student Data from a Text File/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Retrieve_Student_Data_from_a_Text_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                string filePath = "D:\\.net projects\\Practice Project-2/sample.txt";
+
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                Console.WriteLine("Rainbow School Students Data:");
+
+                 foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
